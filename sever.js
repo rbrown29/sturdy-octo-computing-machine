8ede7358825d0d84fbd578f30dc4fa4ee2ae991e
Richard Brown <richardbrown@Richard.hsd1.or.comcast.net>
2019-11-05 12:26:31 -0800

    troubleshooting

--- sever.js
+++ sever.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 // port
 
 const PORT = process.env.PORT
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_Uri;
 mongoose.connect(MONGODB_URI , { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }
 );
 
