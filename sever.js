dce148002d64efbd84d8f404534eb0c64a70c98b
Richard Brown <richardbrown@Richard.hsd1.or.comcast.net>
2019-11-05 21:32:24 -0800

    testing

--- sever.js
+++ sever.js
@@ -1,4 +1,4 @@
-// Dependencies
+////////Dependencies//////////////
 const express = require("express");
 const methodOverride = require("method-override");
 const mongoose = require("mongoose");
@@ -6,26 +6,33 @@ const app = express();
 const db = mongoose.connection;
 
 require('dotenv').config();
-// port
+//// port /////////////////
 
+////////////////////////////////////////////
 const PORT = process.env.PORT
 const MONGODB_URI = process.env.MONGODB_URI;
 mongoose.connect(MONGODB_URI , { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }
 );
+///////////////////////////////////////////
 
+//////////////////////////////////////////
 db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
 db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
 db.on('disconnected', () => console.log('mongo disconnected'));
+///////////////////////////////////////////////////////
 
+/////////////////////////////////////////////////
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(methodOverride('_method'));
+/////////////////////////////////////////////////////
 
+/////////////////////////////////////////////////////
 app.get('/' , (req, res) => {
   res.send('Hello Richard!');
 });
-
+////////////////////////////////////////////
 app.listen(PORT, () => console.log( 'Listening on port:', PORT));
 
 console.log(MONGODB_URI);
